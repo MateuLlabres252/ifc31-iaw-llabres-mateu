6b03ad94cd55138b210fb3ce67e570870259bc32
MateuLlabres252 <mateullabres@paucasesnovescifp.cat>
2026-02-13 17:38:49 +0100

    Update Animal-routes.js

--- routes/Animal-routes.js
+++ routes/Animal-routes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const AnimalController = require('../controllers/Animalcontroller');
 
-// Listar todos los animales
+// Listar todos los animales, también se puede filtrar por tipo
 router.get('/animales', AnimalController.listarAnimales);
 
 // Obtener un animal por nombre
-router.get('/animales/:nombre', AnimalController.getAnimal);
-
+router.get('/animales/nombre/:nombre', AnimalController.getAnimal);
+//por jaula
+router.get('/animales/jaula/:jaula', AnimalController.getJaula);
 module.exports = router;
